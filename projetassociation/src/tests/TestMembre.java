ece92585b45b81ec771d61b433847f0368333980
Jean-Andre Thomas <jean-andre.thomas@etudiant.univ-brest.fr>
2022-11-21 20:36:42 +0000

    Upload New File

---
+++ projetassociation/src/tests/TestMembre.java
@@ -0,0 +1,14 @@
+package tests;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+/**
+ * Tests JUnit de la classe {@link association.Membre.Membre}.
+ *
+ * @author Jean-Andre
+ * @see association.InformationPersonnelle
+ */
+public class TestMembre {
+
+}
