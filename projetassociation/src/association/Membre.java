74d50cb0c1662f938909b1b4e89124abdf3f3927
Thomas Jean-André <jean.andre.thomas22@gmail.com>
2022-11-30 14:50:10 +0100

    1 er envoie eclipse

--- projetassociation/src/association/Membre.java
+++ projetassociation/src/association/Membre.java
@@ -1,9 +1,11 @@
 package association;
 
 import java.util.List;
+import java.util.Objects;
+import javax.xml.catalog.CatalogManager;
 
 /**
- * Gestion du membre ainsi que les évènements du membres et ses informations
+ * Gestion du membre ainsi que les Ã©vÃ¨nements du membres et ses informations
  * personnelles.
  *
  * @author Jean
@@ -11,9 +13,9 @@ import java.util.List;
 public class Membre implements InterMembre {
   
   /**
-   * Les evenements auxquels participe ou participera le membre.
+   * Les evenements auxquels participe ou a participé le membre.
    */
-  private InterGestionEvenements evenements;
+  private List<Evenement> listeEvenements;
   
   /**
    * Les informations personnelles du membre.
@@ -22,7 +24,7 @@ public class Membre implements InterMembre {
   
   
   /**
-   * Creer un membre avec ses informations corrélées à ses informations
+   * Creer un membre avec ses informations corrÃ©lÃ©es Ã  ses informations
    * personnelles.
    *
    * @param info les informations personnelles du membre
@@ -33,38 +35,48 @@ public class Membre implements InterMembre {
   }
   
   /**
-   * La liste des évènements auquel le membre est inscrit ou a participé.
+   * La liste des évènements auquel le membre est inscrit ou a participÃ©.
    *
-   * @return la liste des événements du membre
+   * @return la liste des évènements auxquels participe le membre
    */
   @Override
   public List<Evenement> ensembleEvenements() {
-    return evenements.ensembleEvenements();
+    
+    // regarde pour chaque evenement dans la liste d'évènement
+    for (Evenement e : listeEvenements) {
+      // si la liste contient l'objet infos du membre
+      if (e.getParticipants().contains(infos)) {
+        
+        listeEvenements.add(e);
+      }
+    }
+    return listeEvenements;
   }
   
   /**
-   * La liste des événements auquel le membre est inscrit et qui n'ont pas
+   * La liste des Ã©vÃ©nements auquel le membre est inscrit et qui n'ont pas
    * encore eu lieu.
    *
-   * @return la liste des évènements é venir du membre
+   * @return la liste des Ã©vÃ¨nements Ã© venir du membre
    */
   @Override
   public List<Evenement> ensembleEvenementsAvenir() {
-    return evenements.ensembleEvenementAvenir();
+    
   }
   
   /**
-   * D�finit les informations personnelles (adresse et age) du membre en
-   * v�rifant si c'est bien lui (nom et prénom).
+   * Dï¿½finit les informations personnelles (adresse et age) du membre en
+   * vï¿½rifant si c'est bien lui (nom et prenom).
    *
    * @param info les informations personnelles du membre
    */
   @Override
   public void definirInformationPersonnnelle(InformationPersonnelle info) {
-    if (this.infos.getNom() == info.getNom()
-        && this.infos.getPrenom() == infos.getPrenom()) {
+    if (this.infos.equals(info)) {
       info.setAdresse(this.infos.getAdresse());
       info.setAge(this.infos.getAge());
+    } else if (this.infos != null) {
+      
     }
   }
   
@@ -72,14 +84,52 @@ public class Membre implements InterMembre {
    * Renvoie les informations personnelles du membre.
    *
    * @return l'objet contenant les informations personnelles du membre ou
-   *         <code>null</code> si aucune information n'a été définie.
+   *         <code>null</code> si aucune information n'a Ã©tÃ© dÃ©finie.
    */
   @Override
   public InformationPersonnelle getInformationPersonnelle() {
-    if (infos.getNom() == null) {
+    if (infos == null) {
       return null;
     } else {
       return infos;
     }
   }
+  
+  /**
+   * Retourne un hashcode des infos du membre.
+   */
+  @Override
+  public int hashCode() {
+    return Objects.hash(infos);
+  }
+  
+  /**
+   * Retourne en string les infos du membre.
+   */
+  @Override
+  public String toString() {
+    return infos.getNom() + " " + infos.getPrenom() + " " + infos.getAdresse()
+        + " " + infos.getAge();
+  }
+  
+  
+  @Override
+  public boolean equals(final Object obj) {
+    if (this == obj) {
+      return true;
+    }
+    if (obj == null) {
+      return false;
+    }
+    if (getClass() != obj.getClass()) {
+      return false;
+    }
+    Membre other = (Membre) obj;
+    return Objects.equals(infos.getAdresse(), other.infos.getAdresse())
+        && infos.getAge() == other.infos.getAge()
+        && Objects.equals(infos.getNom(), other.infos.getNom())
+        && Objects.equals(infos.getPrenom(), other.infos.getPrenom());
+  }
+  
 }
+
